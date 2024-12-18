6db951317381d9b099cde1c9ab511229f1e2e4af
StefanoSacchet <sacchet13@gmail.com>
2024-12-18 09:56:44 +0100

    added packet forge

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,9 @@
 use crossbeam::channel::{Receiver, Sender, TryRecvError};
+use packet_forge::{PacketForge, TextMessage};
 use std::{collections::HashMap, thread, time::Duration};
 use wg_internal::controller::{DroneCommand, DroneEvent};
 use wg_internal::network::NodeId;
-use wg_internal::packet::Packet;
+use wg_internal::packet::{Fragment, Packet, PacketType};
 
 pub struct Server {
     id: NodeId,
@@ -10,9 +11,12 @@ pub struct Server {
     command_recv: Receiver<DroneCommand>,
     receiver: Receiver<Packet>,
     senders: HashMap<NodeId, Sender<Packet>>,
+    packet_forge: PacketForge,
+    packets_map: HashMap<u64, Vec<Fragment>>,
 }
 
 impl Server {
+    #[must_use]
     pub fn new(
         id: NodeId,
         command_send: Sender<DroneEvent>,
@@ -26,6 +30,8 @@ impl Server {
             command_recv,
             receiver,
             senders,
+            packet_forge: PacketForge::new(),
+            packets_map: HashMap::new(),
         }
     }
 
@@ -33,15 +39,40 @@ impl Server {
         self.id
     }
 
-    pub fn run(&self) {
+    fn handle_packets(&mut self, packet: Packet) {
+        let session_id = packet.session_id;
+        match packet.pack_type {
+            PacketType::MsgFragment(frag) => {
+                self.packets_map.entry(session_id).or_default().push(frag);
+
+                let fragments = self.packets_map.get(&session_id).unwrap();
+                let total_fragments = fragments[0].total_n_fragments;
+                if fragments.len() as u64 == total_fragments {
+                    let res = self.packet_forge.assemble::<TextMessage>(fragments.clone());
+                    match res {
+                        Ok(msg) => {
+                            println!("Server {} received a text message: {:?}", self.id, msg);
+                        }
+                        Err(err) => {
+                            eprintln!("Error parsing message for server {}: {:?}", self.id, err);
+                        }
+                    }
+                    self.packets_map.remove(&session_id);
+                }
+            }
+            _ => {
+                println!("Server {} received a packet: {:?}", self.id, packet);
+            }
+        }
+    }
+
+    pub fn run(&mut self) {
         loop {
             thread::sleep(Duration::from_secs(1));
 
             // Check if there's a message from the drone
             match self.receiver.try_recv() {
-                Ok(packet) => {
-                    println!("Server {} received a message: {:?}", self.id, packet);
-                }
+                Ok(packet) => self.handle_packets(packet),
                 Err(TryRecvError::Empty) => {
                     println!("No messages for server {}", self.id);
                 }
